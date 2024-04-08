af3824a772adf9cf6de5af8aa9cefb79b2f20b6e
Tom Forman-Kenrick <tom.kenrick@gmail.com>
2024-04-08 11:27:17 +0100

    updated background colours for email and location sections

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// TODO add a mouseover event listener
-let eghamButton = document.getElementsByClassName("button-0")[0];
-
-eghamButton.addEventListener("click", (event) => {
-  eghamButton.textContent = "EGGS and HAM";
-});
-
 // capture email address when email signup button is clicked
 
 const emailSignupButton = document.querySelector(".email-signup__button");
